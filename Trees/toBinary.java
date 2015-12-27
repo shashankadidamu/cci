394850b0159fe7c1eed746e8ae2e02681260c80d
shashankadidamu <adidamu.shashank@gmail.com>
2015-12-27 11:16:31 -0600

    tobinary

---
+++ Trees/toBinary.java
@@ -0,0 +1,36 @@
+
+
+class toBinary{
+
+ static String printBinary(double num){
+	
+	if(num>=1 || num <=0)
+		return "ERROR";
+
+	StringBuilder binary = new StringBuilder();
+
+	binary.append(".");
+
+	while(num>0){
+
+		if(binary.length()>=32)
+			return "ERROR";
+
+		double r=num*2;
+		if(r>=1){
+			binary.append(1);
+			num=r-1;
+		}
+		else{
+			binary.append(0);
+			num=r;
+		}
+	}
+	return binary.toString();
+}
+
+	public static void main(String[] args){
+		String result= printBinary(1.101);
+		System.out.println(result);
+	}
+}
